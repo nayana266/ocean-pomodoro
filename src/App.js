455ce6dc4fbf4a2c0847193d702bb9b06750f4f7
nayana266 <nayananona@gmail.com>
2025-03-29 18:49:39 -0700

    button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-// App.js
 import React from "react";
-import "./style.css"; 
-
-import { Label } from "./Label";  // Correct named import
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom"; // Correct import of useNavigate
+import "./style.css";
+import { Label } from "./Label"; // Correct named import
 import Image from "./Image";
-
-
-import logo from './logo.svg';
-import Timer from "./Timer";
 import './App.css';
 
 function App() {
-
   return (
-    <div className="App">
-            <Image />
-            <Label />
-            
-        </div>
-    
+    <Router> {/* Wrap the entire app in Router */}
+      <div className="App">
+        <Image />
+        <Label />
+        <StartButton /> {/* StartButton component that handles navigation */}
+
+        {/* Define Routes inside Router */}
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Default route for the home page */}
+          <Route path="/newpage" element={<NewPage />} /> {/* Route to new page */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
+// Home Page Component (Optional)
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>This is the starting page of your app.</p>
+    </div>
+  );
+}
+
+// StartButton Component - Navigates to the new page on click
+function StartButton() {
+  const navigate = useNavigate(); // Hook to navigate to new page
+
+  const handleButtonClick = () => {
+    navigate("/newpage"); // Navigate to '/newpage' on button click
+  };
+
+  return (
+    <button className="start-button" onClick={handleButtonClick}>
+      Start
+    </button>
+  );
+}
+
+// NewPage Component - Content shown when '/newpage' is visited
+function NewPage() {
+  console.log('Rendering NewPage'); // This will help you check if the component is mounted
+  return (
+    <div>
+      <h1>Welcome to the New Page!</h1>
+      <p>This is the content of the new page!</p>
+    </div>
+  );
+}
 
 export default App;
