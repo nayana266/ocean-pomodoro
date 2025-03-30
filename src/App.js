0406687fb813480654d2eb7fe1ea447bf29268ce
riyanalla <riyathenalla@gmail.com>
2025-03-29 23:51:04 -0700

    Cute add-ins

--- src/App.js
+++ src/App.js
@@ -8,11 +8,53 @@ import Timer from "./Timer";
 function App() {
   const [start, setStart] = useState(false); // State to track the start button click
 
+  const seashellStyle = {
+    position: "absolute",
+    top: "30px", // Adjust as needed
+    left: "50px", // Adjust as needed
+    transform: "rotate(30deg)", // Adjust rotation as needed
+    width: "100px",
+    height: "100px",
+  };
+
+  const coralStyle = {
+    position: "absolute",
+    top: "30%", // Adjust as needed
+    left: "20px", // Adjust as needed
+    transform: "rotate(250deg)", // Adjust rotation as needed
+    width: "100px",
+    height: "100px",
+  };
+
+  const coral2Style = {
+    position: "absolute",
+    bottom: "10px", // Adjust as needed
+    right: "10px", // Adjust as needed
+    transform: "rotate(0deg)", // No rotation
+    width: "100px",
+    height: "100px",
+  };
+
+  const starStyle = {
+    position: "absolute",
+    bottom: "10px", // Adjust as needed
+    left: "10px", // Adjust as needed
+    transform: "rotate(305deg)", // Adjust rotation as needed
+    width: "100px",
+    height: "100px",
+  };
+
   return (
     <div className="App">
       {/* Display the Image and Label components */}
       <Image />
       <Label />
+
+      {/* Adds seashell, coral, and star as decorations */}
+      <img src="/icons/blueshell.png" alt="Seashell" className="seashell" style= {seashellStyle} />
+      <img src="/icons/coral.png" alt="Coral1" className="coral" style={coralStyle}/>
+      <img src="/icons/pixelcoral.png" alt="Coral2" className="coral2" style={coral2Style}/>
+      <img src="/icons/star.png" alt="Star" className="star" style={starStyle}/>
 
       {/* Conditionally render Timer based on the start state */}
       {start ? (
